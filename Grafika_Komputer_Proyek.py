14f4dee7095cba8c22e4ac6ec9e643355ada87dd
RendyRensFirmansyah <rendyfirmansyah.job@gmail.com>
2022-11-26 12:22:01 +0700

    Asset sama background bangunanya wes tak gabungin rek, tinggal ngepoles kota kota nya, sama ngejalanin pesawate

--- Grafika_Komputer_Proyek.py
+++ Grafika_Komputer_Proyek.py
@@ -1,6 +1,13 @@
+import OpenGL.GLUT as glut
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
+import math
+import random
+
+#Position awal pesawat
+x_body = 0
+y_body = -100
 
 pos_x_tombol = 0
 pos_y_tombol = -200
@@ -20,18 +27,222 @@ def init():
     glClearColor(0.0, 0.0, 0.0, 1.0)
     gluOrtho2D(-500.0, 500.0, -500.0, 500.0)
 
-def tombolPlay():
+def drawTextBold(ch,xpos,ypos):
+    glPushMatrix()
+    font_style = glut.GLUT_BITMAP_HELVETICA_18
+    glColor3ub(255,255,255)
+    line=0
+    glRasterPos2f (xpos, ypos)
+    for i in ch:
+       if  i=='\n':
+          line=line+1
+          glRasterPos2f (xpos, ypos*line)
+       else:
+          glutBitmapCharacter(font_style, ord(i))  
+    glPopMatrix()
+    
+def lingkaranPlay(xn,yn,sugmentyn):
+    
+    glColor3ub(0,0,0)
+    glBegin(GL_POLYGON)
+    for i in range(sugmentyn):
+        theta= 2 *3.1415926*i/sugmentyn
+        x = 64* math.cos(theta)
+        y = 64* math.sin(theta)
+        glVertex2f(x + xn, y + yn)
+    glEnd()
+    
+def tombolPlay(cx,cy,num_seg):
     global pos_x_tombol, pos_y_tombol
     glPushMatrix()
     glColor3ub(102, 255, 0)
     glTranslated(pos_x_tombol,pos_y_tombol,0)
+    
+    glBegin(GL_POLYGON)
+    for i in range(num_seg):
+        theta= 2 *3.1415926*i/num_seg
+        x = 100* math.cos(theta)
+        y = 100* math.sin(theta)
+        glVertex2f(x + cx, y + cy)
+    glEnd()
+    
+    glColor3ub(0, 0, 0)
+    glBegin(GL_POLYGON)
+    glVertex2f(-20,52)#c
+    glVertex2f(-20,-52)#d
+    glVertex2f(-5,-56)#k
+    glVertex2f(38,-5)#n
+    glVertex2f(38,5)#e
+    glVertex2f(-5,56)#j
+    # glBegin(GL_QUADS)
+    # glVertex2f(-20,-20)
+    # glVertex2f(20,-20)
+    # glVertex2f(20,20)
+    # glVertex2f(-20,20)
 
-    glBegin(GL_QUADS)
-    glVertex2f(-20,-20)
-    glVertex2f(20,-20)
-    glVertex2f(20,20)
-    glVertex2f(-20,20)
+    glEnd()
+    glPopMatrix()
 
+#Player Pesawat
+def bodyAir(cx,cy,num_segment):
+    glPushMatrix()
+    glScale(0.5,0.5,0)
+    global y_body
+    glTranslatef(-700,-300,0)
+    glTranslated(x_body, y_body, 0)
+    #Body Pesawat
+    glColor3ub(240,240,240)
+    # glLineWidth(3)
+    glBegin(GL_POLYGON)
+    for i in range(num_segment):
+        theta= 2 *3.1415926*i/num_segment
+        x = 6* math.cos(theta)
+        y = 6 * math.sin(theta)
+        glVertex2f(x + cx, y + cy)
+    glEnd()
+    
+    #Body Pesawat
+    glColor3ub(240,240,240)
+    glBegin(GL_POLYGON)
+    glVertex2f(77,5)#l
+    glVertex2f(80,16)#c
+    glVertex2f(50,40)#d
+    glVertex2f(-100,40)#n
+    glColor3ub(240,0,0)
+    glVertex2f(-115,5)#k
+    glEnd()
+    glColor3ub(240,240,240)
+    glBegin(GL_POLYGON)
+    glVertex2f(-100,40)#n
+    glColor3ub(240,0,0)
+    glVertex2f(-120,80)#f
+    glVertex2f(-140,80)#g
+    glVertex2f(-140,35)#h
+    glVertex2f(-130,25)#i
+    glVertex2f(-140,25)#j
+    glVertex2f(-140,15)#m
+    glVertex2f(-115,5)#k
+    glEnd()
+    
+    #Body Sayap Depan Pesawat
+    glColor3ub(240,240,240)
+    glBegin(GL_POLYGON)
+    glVertex2f(20,10)#e
+    glVertex2f(-15,10)#o
+    glVertex2f(-50,-20)#q
+    glVertex2f(-75,-25)#r
+    glColor3ub(240,0,0)
+    glVertex2f(-20,-25)#s
+    glEnd()
+    #Body Sayap Belakang Pesawat
+    glColor3ub(240,240,240)
+    glBegin(GL_POLYGON)
+    glVertex2f(-80,10)#t
+    glVertex2f(-110,10)#u
+    glVertex2f(-140,-10)#v
+    glColor3ub(240,0,0)
+    glVertex2f(-110,-10)#w
+    glEnd()
+    
+    #Cendela Pesawat Pilot
+    glColor3ub(0,0,0)
+    glBegin(GL_POLYGON)
+    glVertex2f(65,28)#z
+    glVertex2f(57,34)#a1
+    glVertex2f(42,34)#b1
+    glVertex2f(42,28)#c1
+    glEnd()
+    
+    #Pintu Pesawat Depan
+    glColor3ub(0,0,0)
+    glBegin(GL_POLYGON)
+    glVertex2f(36,36)#p
+    glVertex2f(36,22)#d1
+    glVertex2f(28,22)#e1
+    glVertex2f(28,36)#f1
+    glEnd()
+    
+    #Pintu Pesawat Belakang
+    glColor3ub(0,0,0)
+    glBegin(GL_POLYGON)
+    glVertex2f(-36,22)#g2
+    glVertex2f(-36,36)#h2
+    glVertex2f(-44,36)#i2
+    glVertex2f(-44,22)#j2
+    glEnd()
+    
+    #Cendela penumpang Pesawat
+    glColor3ub(0,0,0)
+    glBegin(GL_POLYGON)
+    glVertex2f(24,30)#h1
+    glVertex2f(24,34)#g1
+    glVertex2f(20,34)#j1
+    glVertex2f(20,30)#i1
+    glEnd()
+    glColor3ub(0,0,0)
+    glBegin(GL_POLYGON)
+    glVertex2f(14,30)#k1
+    glVertex2f(14,34)#l1
+    glVertex2f(10,34)#m1
+    glVertex2f(10,30)#n1
+    glEnd()
+    glColor3ub(0,0,0)
+    glBegin(GL_POLYGON)
+    glVertex2f(4,30)#o1
+    glVertex2f(4,34)#p1
+    glVertex2f(0,34)#q1
+    glVertex2f(0,30)#r1
+    glEnd()
+    glColor3ub(0,0,0)
+    glBegin(GL_POLYGON)
+    glVertex2f(-6,30)#s1
+    glVertex2f(-6,34)#t1
+    glVertex2f(-10,34)#u1
+    glVertex2f(-10,30)#v1
+    glEnd()
+    glColor3ub(0,0,0)
+    glBegin(GL_POLYGON)
+    glVertex2f(-16,30)#w1
+    glVertex2f(-16,34)#z1
+    glVertex2f(-20,34)#a2
+    glVertex2f(-20,30)#b2
+    glEnd()
+    glColor3ub(0,0,0)
+    glBegin(GL_POLYGON)
+    glVertex2f(-26,30)#c2
+    glVertex2f(-26,34)#d2
+    glVertex2f(-30,34)#e2
+    glVertex2f(-30,30)#f2
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex2f(-48,30)#k2
+    glVertex2f(-48,34)#l2
+    glVertex2f(-52,34)#m2
+    glVertex2f(-52,30)#n2
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex2f(-58,30)#o2
+    glVertex2f(-58,34)#p2
+    glVertex2f(-62,34)#q2
+    glVertex2f(-62,30)#r2
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex2f(-68,30)#s2
+    glVertex2f(-68,34)#t2
+    glVertex2f(-72,34)#u2
+    glVertex2f(-72,30)#v2
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex2f(-78,30)#w2
+    glVertex2f(-78,34)#z2
+    glVertex2f(-82,34)#a3
+    glVertex2f(-82,30)#b3
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex2f(-88,30)#c3
+    glVertex2f(-88,34)#d3
+    glVertex2f(-92,34)#e3
+    glVertex2f(-92,30)#f3
     glEnd()
     glPopMatrix()
 
@@ -252,13 +463,15 @@ def bangunan4():
     glEnd()
     glPopMatrix()
 
+
 def gamestart():
     background()
+    bodyAir(75,11,360)
     bangunan1()
     bangunan2()
     bangunan3()
     bangunan4()
-
+        
 def display():
     glClear(GL_COLOR_BUFFER_BIT)
     glColor3f(1.0,1.0,1.)
@@ -267,7 +480,10 @@ def display():
     if gameMulai == True:
         gamestart()
     else:
-        tombolPlay()
+        lingkaranPlay(-12,52,360)#bagian atas
+        lingkaranPlay(-12,-52,360)#bagian bawah
+        lingkaranPlay(32,0,360)#bagian tengah
+        tombolPlay(0,0,360)
     glFlush()
 
 def input_untuk_mouse(button, state, x,y):
@@ -277,15 +493,18 @@ def input_untuk_mouse(button, state, x,y):
         gameMulai = True
     
 def input_keyboard(key,x,y):
-    global pos_x_kotak, pos_y_kotak
+    global y_body
     if key == GLUT_KEY_UP:
-        pos_y_kotak = 10
+        if y_body == 440:
+            y_body +=0
+        else:
+            y_body += 10
+            
     elif key == GLUT_KEY_DOWN:
-        pos_y_kotak -= 5
-    elif key == GLUT_KEY_RIGHT:
-        pos_x_kotak += 5
-    elif key == GLUT_KEY_LEFT:
-        pos_x_kotak -= 5
+        if y_body == 60:
+            y_body -=0
+        else:
+            y_body -= 10
 
 def update(value):
     glutPostRedisplay()
@@ -295,10 +514,11 @@ def main():
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB)
     glutInitWindowSize(1280,550)
-    glutInitWindowPosition(100,100)
-    glutCreateWindow("collision")
+    glutInitWindowPosition(0,0)
+    glutCreateWindow("Game Pesawat")
     glutDisplayFunc(display)
     glutSpecialFunc(input_keyboard)
+    # glutSpecialFunc(positionPesawat)
     glutMouseFunc(input_untuk_mouse)
     glutTimerFunc(1,update,0)
     init()
